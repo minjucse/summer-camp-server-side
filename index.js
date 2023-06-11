24d3adc1fc487d41fb8bc4d118198512994a72bd
minjucse <minjucse@gmail.com>
2023-06-11 22:07:51 +0600

    ACCESS TOKEN  error solve

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ const verifyJWT = (req, res, next) => {
   // bearer token
   const token = authorization.split(' ')[1];
 
-  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+  jwt.verify(token, process.env.ACCESS_TOKEN, (err, decoded) => {
     if (err) {
       return res.status(401).send({ error: true, message: 'unauthorized access' })
     }

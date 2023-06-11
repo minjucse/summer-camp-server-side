c9ca48991cec47dd7768faa5c54b1141d0fb8359
minjucse <minjucse@gmail.com>
2023-06-11 22:03:53 +0600

    jwt method error solve

--- index.js
+++ index.js
@@ -52,7 +52,7 @@ async function run() {
 
     app.post('/jwt', (req, res) => {
       const user = req.body;
-      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '24h' })
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN, { expiresIn: '24h' })
 
       res.send({ token })
     })
